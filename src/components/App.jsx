3f5a84de49c3984567807c81e18788f9fe8be6e7
EspFerevo <olekchekrenov@gmail.com>
2024-06-24 21:46:40 +0300

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
-import { Layout } from "./Layout/Layout";
-import { AppBar } from "./AppBar/AppBar";
-import { TaskForm } from "./TaskForm/TaskForm";
-import { TaskList } from "./TaskList/TaskList";
+// import { Layout } from "./Layout/Layout";
+// import { AppBar } from "./AppBar/AppBar";
+// import { TaskForm } from "./TaskForm/TaskForm";
+// import { TaskList } from "./TaskList/TaskList";
+
+// export const App = () => {
+//   return (
+//     <Layout>
+//       <AppBar />
+//       <TaskForm />
+//       <TaskList />
+//     </Layout>
+//   );
+// };
 
 export const App = () => {
-  return (
-    <Layout>
-      <AppBar />
-      <TaskForm />
-      <TaskList />
-    </Layout>
-  );
+  return;
 };
