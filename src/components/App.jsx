9f0d4648712f3e965f896788fd60326a13fbbf3c
EspFerevo <olekchekrenov@gmail.com>
2024-06-25 20:11:49 +0300

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,14 @@
-// import { Layout } from "./Layout/Layout";
-// import { AppBar } from "./AppBar/AppBar";
-// import { TaskForm } from "./TaskForm/TaskForm";
-// import { TaskList } from "./TaskList/TaskList";
-
-// export const App = () => {
-//   return (
-//     <Layout>
-//       <AppBar />
-//       <TaskForm />
-//       <TaskList />
-//     </Layout>
-//   );
-// };
+import { Layout } from "./Layout/Layout";
+import { AppBar } from "./AppBar/AppBar";
+import { TaskForm } from "./TaskForm/TaskForm";
+import { TaskList } from "./TaskList/TaskList";
 
 export const App = () => {
-  return;
+  return (
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      <TaskList />
+    </Layout>
+  );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { Provider } from "react-redux";
-import { App } from "./components/App";
-import { store } from "./redux/store";
 import ReactDOM from "react-dom/client";
+import { App } from "./components/App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
